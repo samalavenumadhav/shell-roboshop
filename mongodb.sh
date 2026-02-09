8b4cc34c7070e29707af3c016cd9006169890fce
venu madhav <madhavsamala171@gmail.com>
2026-02-09 14:39:40 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,23 @@
+#!/bin/bas
+
+USERId=$(id -u)
+LOGS_FOLDER="var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if (USERID -ne 0); then
+    echo -e "$ Please Run This Script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if (USERID -ne 0); then
+        echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
+}
