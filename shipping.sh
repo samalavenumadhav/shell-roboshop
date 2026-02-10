4d8d48357ca71f5489cb5c7e6747618198be12bb
venu madhav <madhavsamala171@gmail.com>
2026-02-10 16:18:54 +0530

    Actions

--- shipping.sh
+++ shipping.sh
@@ -19,6 +19,8 @@ mkdir -p $LOGS_FOLDER
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
 }
 
 dnf install maven -y &>>LOGS_FILE
