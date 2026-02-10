b72aa36ff72248df95a6a0de1e8891cf7ca75629
venu madhav <madhavsamala171@gmail.com>
2026-02-10 16:56:22 +0530

    Actions

---
+++ payment.sh
@@ -0,0 +1,54 @@
+#1/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y &>>LOGS_FILE
+VALIDATE $? "Installing Python"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
+VALIDATE $? "Creating System User"
+
+mkdir /app 
+VALIDATE $? "Creating Directory"
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>LOGS_FILE
+VALIDATE $? "Downloading Payment Zip Code"
+
+cd /app 
+unzip /tmp/payment.zip &>>LOGS_FILE
+VALIDATE $? "Unzip Payment Code"
+
+cd /app 
+pip3 install -r requirements.txt &>>LOGS_FILE
+VALIDATE $? "Installing Python Client Package"
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "Created Systemctl Service"
+
+systemctl daemon-reload
+systemctl enable payment &>>LOGS_FILE
+systemctl start payment
+VALIDATE $? "Enable and Start Payment"
---
+++ rabbitmq.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+        echo -e "R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Coping Rabbitmq Repo"
+
+dnf install rabbitmq-server -y &>>LOGS_FILE
+VALIDATE $? "Installing Rabbitmq Server"
+
+systemctl enable rabbitmq- &>>LOGS_FILE
+systemctl start rabbitmq-server
+VALIDATE $? "Enable and start Rabbitmq Server"
+
+rabbitmqctl add_user roboshop roboshop123 &>>LOGS_FILE
+VALIDATE $? "Creating User"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &LOGS_FILE
+VALIDATE $? "Setting Permissions"
