30bcb3001803ce455ad9816d86fbc0b4d33d96fd
venu madhav <madhavsamala171@gmail.com>
2026-02-10 13:02:10 +0530

    Actions

--- cart.sh
+++ cart.sh
@@ -35,7 +35,7 @@ dnf install nodejs -y &>>LOGS_FILE
 VALIDATE $? "Installing NodeJS"
 
 id roboshop &>>$LOGS_FILE
-if [ $? -ne o ]; then 
+if [ $? -ne 0 ]; then 
 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "Creating Systen User"
