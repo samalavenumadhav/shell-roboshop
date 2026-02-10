f1d51662b271e759b764a1d7869bcb74c3073d1e
venu madhav <madhavsamala171@gmail.com>
2026-02-10 16:15:41 +0530

    Actions

---
+++ shipping.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+if [ $USERID -ne 0 ]; then      
+        echo -e $R Plerase run this script with root user acccess $N | tee -a $LOGS_FILE
+        exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+}
+
+dnf install maven -y &>>LOGS_FILE
+VALIDATE $? "Installing Maven"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
+VALIDATE $? "Creating System User"
+
+mkdir /app 
+VALIDATE $? "Creating Directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>LOGS_FILE
+VALIDATE $? "Downloading Zip Code"
+
+cd /app 
+unzip /tmp/shipping.zip &>>LOGS_FILE
+VALIDATE $? "Unzip Shipping Code"
+
+cd /app 
+mvn clean package  &>>LOGS_FILE
+VALIDATE $? "Installing Dependies"
+
+mv target/shipping-1.0.jar shipping.jar &>>LOGS_FILE
+VALIDATE $? "Changing File Name"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>LOGS_FILE
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable shipping &>>LOGS_FILE
+systemctl start shipping
+VALIDATE $? "Enable and Start Shipping"
+
+dnf install mysql -y &>>LOGS_FILE
+VALIDATE $? "Installing Mysql Client"
+
+mysql -h mysql.samala.online -uroot -pRoboShop@1 < /app/db/schema.sql &>>LOGS_FILE
+VALIDATE $? "Schema"
+
+mysql -h mysql.samala.online -uroot -pRoboShop@1 < /app/db/app-user.sql &>>LOGS_FILE
+VALIDATE $? "user"
+
+mysql -h mysql.samala.online -uroot -pRoboShop@1 < /app/db/master-data.sql &>>LOGS_FILE
+VALIDATE $? "master"
+
+systemctl restart shipping 
+VALIDATE $? "Restarting Shipping"
