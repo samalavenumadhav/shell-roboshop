57b17e6a1eb6a96bdbfaff7c982cf4d49178c734
venu madhav <madhavsamala171@gmail.com>
2026-02-19 12:41:21 +0530

    actions

--- payment.sh
+++ payment.sh
@@ -26,12 +26,12 @@ VALIDATE(){
     fi
 }
 
-dnf install python3 gcc python3-devel -y &>>LOGS_FILE
+dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
 VALIDATE $? "Installing Python"
 
-id roboshop &>>LOGS_FILE
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating System User"
 else
     echo -e "Roboshop user already exit....$Y SKIPPING $N" 
@@ -40,26 +40,26 @@ fi
 mkdir -p /app 
 VALIDATE $? "Creating Directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>LOGS_FILE
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading Payment Zip Code"
 
 cd /app 
 VALIDATE $? "Moving to App Directory"
 
 rm -rf /app/*
-VALIDATE $? "Removing Existing Code" &>>LOGS_FILE
+VALIDATE $? "Removing Existing Code" &>>$LOGS_FILE
 
-unzip /tmp/payment.zip &>>LOGS_FILE
+unzip /tmp/payment.zip &>>$LOGS_FILE
 VALIDATE $? "Unzip Payment Code"
 
 cd /app 
-pip3 install -r requirements.txt &>>LOGS_FILE
+pip3 install -r requirements.txt &>>$LOGS_FILE
 VALIDATE $? "Installing Python Client Package"
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 VALIDATE $? "Created Systemctl Service"
 
 systemctl daemon-reload
-systemctl enable payment &>>LOGS_FILE
+systemctl enable payment &>>$LOGS_FILE
 systemctl start payment
 VALIDATE $? "Enable and Start Payment"
